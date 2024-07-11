0577a61ad99a9f7cdd692df9e3f6f2a78b4a73f9
Guilherme Câmara <guilherme.camara@looplex.com.br>
2024-07-11 13:38:08 -0300

    fix(core.webapi): added default ext methods to add profiles and services

---
+++ src/Looplex.DotNet.Core.WebAPI/ExtensionMethods/ServicesExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Looplex.DotNet.Core.Infra.IoC;
+using Looplex.DotNet.Core.Infra.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Looplex.DotNet.Core.WebAPI.ExtensionMethods;
+
+public static class ServicesExtensionMethods
+{
+    public static void AddCoreAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(CoreProfile));
+    }
+
+    public static void AddCoreServices(this IServiceCollection services)
+    {
+        CoreDependencyContainer.RegisterServices(services);
+    }
+}
