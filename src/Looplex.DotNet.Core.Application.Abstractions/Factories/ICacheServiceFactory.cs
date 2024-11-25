9665a9bfb3efd2e8675893628fc1e9a76d33b911
Guilherme Câmara <guilherme.camara@outlook.com.br>
2024-11-25 20:29:12 -0300

    fix(cache): added cache service factory

---
+++ src/Looplex.DotNet.Core.Application.Abstractions/Factories/ICacheServiceFactory.cs
@@ -0,0 +1,9 @@
+﻿using Looplex.DotNet.Core.Application.Abstractions.Services;
+
+namespace Looplex.DotNet.Core.Application.Abstractions.Factories;
+
+public interface ICacheServiceFactory
+{
+    void RegisterCacheService(string name, ICacheService cacheService);
+    ICacheService GetCacheService(string name);
+}
--- src/Looplex.DotNet.Core.Application.Abstractions/Services/ICacheService.cs
+++ src/Looplex.DotNet.Core.Application.Abstractions/Services/ICacheService.cs
@@ -7,6 +7,6 @@ public interface ICacheService
 {
     Task SetCacheValueAsync(string key, string value, TimeSpan? expiry = null);
     Task<bool> ContainsKeyAsync(string key);
-    Task<bool> TryGetCacheValueAsync(string key, out string value);
+    Task<bool> TryGetCacheValueAsync(string key, out string? value);
     Task RemoveCacheValueAsync(string key);
 }
