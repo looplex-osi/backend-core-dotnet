d8b56232aad87ad75c3cee212985e8f6b5d90573
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-01-14 17:22:08 -0300

    feat(irbacservice): moved rbacservice interface from middlewares repo to this repo

---
+++ src/Looplex.DotNet.Core.Application.Abstractions/Services/IRBACService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Looplex.OpenForExtension.Abstractions.Contexts;
+
+namespace Looplex.DotNet.Core.Application.Abstractions.Services;
+
+public enum RbacAction
+{
+    Read,
+    Write,
+    Delete
+}
+
+/// <summary>
+/// Provides role-based access control (RBAC) functionality for authorization checks.
+/// </summary>
+public interface IRbacService
+{
+    /// <summary>
+    /// Checks if the user in the context has permission to perform an action on
+    /// a resource within a domain (also from context).
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="resource"></param>
+    /// <param name="action"></param>
+    /// <param name="cancellationToken"></param>
+    void ThrowIfUnauthorized(IContext context, string resource, RbacAction action, CancellationToken cancellationToken);
+}
