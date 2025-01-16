bee25c43359ac18dc64855ee67e6c80b9b3a802a
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-01-16 10:53:01 -0300

    fix(icrudservice): removed cancellation token from methods in interface
    the cancellation token should already be available in the context state

--- src/Looplex.DotNet.Core.Application.Abstractions/Services/ICrudService.cs
+++ src/Looplex.DotNet.Core.Application.Abstractions/Services/ICrudService.cs
@@ -1,16 +1,15 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Looplex.OpenForExtension.Abstractions.Contexts;
 
 namespace Looplex.DotNet.Core.Application.Abstractions.Services
 {
     public interface ICrudService
     {
-        Task GetAllAsync(IContext context, CancellationToken cancellationToken);
-        Task GetByIdAsync(IContext context, CancellationToken cancellationToken);
-        Task CreateAsync(IContext context, CancellationToken cancellationToken);
-        Task UpdateAsync(IContext context, CancellationToken cancellationToken);
-        Task PatchAsync(IContext context, CancellationToken cancellationToken);
-        Task DeleteAsync(IContext context, CancellationToken cancellationToken);
+        Task GetAllAsync(IContext context);
+        Task GetByIdAsync(IContext context);
+        Task CreateAsync(IContext context);
+        Task UpdateAsync(IContext context);
+        Task PatchAsync(IContext context);
+        Task DeleteAsync(IContext context);
     }
 }
