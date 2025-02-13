e5d2ce19e8fad3c76f3ae68d9e6078df68408192
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-02-13 18:40:01 -0300

    fix(databaseservice): added query multiple async

--- src/Looplex.DotNet.Core.Application.Abstractions/Services/ISqlDatabaseService.cs
+++ src/Looplex.DotNet.Core.Application.Abstractions/Services/ISqlDatabaseService.cs
@@ -8,14 +8,16 @@ namespace Looplex.DotNet.Core.Application.Abstractions.Services;
 
 public interface ISqlDatabaseService : IDisposable
 {
-    Task<int> ExecuteAsync(string sql, object? parameters = null, IDbTransaction? transaction = null);
+    Task<int> ExecuteAsync(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
 
-    Task<IEnumerable<T>> QueryAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null);
+    Task<IEnumerable<T>> QueryAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
 
-    Task<IEnumerable<TR>> QueryAsync<TF, TS, TR>(string sql, Func<TF, TS, TR> map, object? parameters = null, IDbTransaction? transaction = null, string splitOn = "Id");
+    Task<IEnumerable<TR>> QueryAsync<TF, TS, TR>(string sql, Func<TF, TS, TR> map, object? parameters = null, IDbTransaction? transaction = null, string splitOn = "Id", CommandType? commandType = null);
 
-    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null);
+    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
 
+    Task<(IEnumerable<T1>, IEnumerable<T2>)> QueryMultipleAsync<T1, T2>(string sql, object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+    
     DbTransaction BeginTransaction();
 
     void OpenConnection();
