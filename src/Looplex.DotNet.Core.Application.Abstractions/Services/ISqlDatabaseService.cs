2e5d974bfcb4a0967a99d242acc7191822b7f701
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-02-14 09:26:57 -0300

    fix(database): added up to 9 result sets on querymultiple for databaseservice

--- src/Looplex.DotNet.Core.Application.Abstractions/Services/ISqlDatabaseService.cs
+++ src/Looplex.DotNet.Core.Application.Abstractions/Services/ISqlDatabaseService.cs
@@ -8,16 +8,50 @@ namespace Looplex.DotNet.Core.Application.Abstractions.Services;
 
 public interface ISqlDatabaseService : IDisposable
 {
-    Task<int> ExecuteAsync(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+    Task<int> ExecuteAsync(string sql, object? parameters = null, IDbTransaction? transaction = null,
+        CommandType? commandType = null);
 
-    Task<IEnumerable<T>> QueryAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+    Task<IEnumerable<T>> QueryAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null,
+        CommandType? commandType = null);
 
-    Task<IEnumerable<TR>> QueryAsync<TF, TS, TR>(string sql, Func<TF, TS, TR> map, object? parameters = null, IDbTransaction? transaction = null, string splitOn = "Id", CommandType? commandType = null);
+    Task<IEnumerable<TR>> QueryAsync<TF, TS, TR>(string sql, Func<TF, TS, TR> map, object? parameters = null,
+        IDbTransaction? transaction = null, string splitOn = "Id", CommandType? commandType = null);
 
-    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? parameters = null, IDbTransaction? transaction = null,
+        CommandType? commandType = null);
 
-    Task<(IEnumerable<T1>, IEnumerable<T2>)> QueryMultipleAsync<T1, T2>(string sql, object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
-    
+    Task<IEnumerable<T1>> QueryMultipleAsync<T1>(string sql, object? param = null, IDbTransaction? transaction = null,
+        CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>)> QueryMultipleAsync<T1, T2>(string sql, object? param = null,
+        IDbTransaction? transaction = null, CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>)> QueryMultipleAsync<T1, T2, T3>(string sql,
+        object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>)> QueryMultipleAsync<T1, T2, T3, T4>(
+        string sql, object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>)>
+        QueryMultipleAsync<T1, T2, T3, T4, T5>(string sql, object? param = null, IDbTransaction? transaction = null,
+            CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>)>
+        QueryMultipleAsync<T1, T2, T3, T4, T5, T6>(string sql, object? param = null, IDbTransaction? transaction = null,
+            CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>,
+        IEnumerable<T7>)> QueryMultipleAsync<T1, T2, T3, T4, T5, T6, T7>(string sql, object? param = null,
+        IDbTransaction? transaction = null, CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>,
+        IEnumerable<T7>, IEnumerable<T8>)> QueryMultipleAsync<T1, T2, T3, T4, T5, T6, T7, T8>(string sql,
+        object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+
+    Task<(IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>,
+        IEnumerable<T7>, IEnumerable<T8>, IEnumerable<T9>)> QueryMultipleAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+        string sql, object? param = null, IDbTransaction? transaction = null, CommandType? commandType = null);
+
     DbTransaction BeginTransaction();
 
     Task OpenConnectionAsync();
