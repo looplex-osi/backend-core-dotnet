8f05cb3efb6638b768460d5504ae329ca57116fa
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-02-20 15:39:48 -0300

    fix(database): removed sqlservice and added a provider for the connection

---
+++ src/Looplex.DotNet.Core.Application.Abstractions/Providers/IDbConnectionProvider.cs
@@ -0,0 +1,9 @@
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Looplex.DotNet.Core.Application.Abstractions.Providers;
+
+public interface IDbConnectionProvider
+{
+    Task<IDbConnection> GetConnectionAsync(string domain);
+}
--- src/Looplex.DotNet.Core.Application.Abstractions/Providers/ISqlDatabaseProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Looplex.DotNet.Core.Application.Abstractions.Services;
-
-namespace Looplex.DotNet.Core.Application.Abstractions.Providers;
-
-public interface ISqlDatabaseProvider
-{
-    Task<ISqlDatabaseService> GetDatabaseAsync(string domain);
-}
