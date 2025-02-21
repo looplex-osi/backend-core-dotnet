f27fc9f99a63621d221b63d905041ceabf09fc5d
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-02-21 09:21:11 -0300

    fix(database): added output var with databasename
    since closing and opening connection resets the database, the use database must be called from the
    caller of GetConnectionAsync. so now the method also returns the database name

--- src/Looplex.DotNet.Core.Application.Abstractions/Providers/IDbConnectionProvider.cs
+++ src/Looplex.DotNet.Core.Application.Abstractions/Providers/IDbConnectionProvider.cs
@@ -5,5 +5,5 @@ namespace Looplex.DotNet.Core.Application.Abstractions.Providers;
 
 public interface IDbConnectionProvider
 {
-    Task<IDbConnection> GetConnectionAsync(string domain);
+    Task<IDbConnection> GetConnectionAsync(string domain, out string databaseName);
 }
